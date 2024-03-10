20e2188f3588bd004281b302b92e0b78fdc74e1e
mhmd-k <mhmdk425@gmail.com>
2024-03-10 04:22:13 +0300

    updated the signup api and finished the login api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -22,7 +22,10 @@ class UserController extends Controller
             ]);
 
             if ($validator->fails()) {
-                return response()->json(['error' => $validator->errors()->first(), 'status' => 400], Response::HTTP_BAD_REQUEST);
+                return response()->json([
+                    'message' => $validator->errors()->first(),
+                    'status' => 400
+                ], Response::HTTP_BAD_REQUEST);
             }
 
             // Create the user
@@ -32,15 +35,86 @@ class UserController extends Controller
             $user->password = Hash::make($request->password);
             $user->save();
 
+            $token = base64_encode($request->name . ":" . $request->password);
 
-            return response()->json(['message' => 'User registered successfully', 'userId' => $user->id, 'status' => 200], Response::HTTP_CREATED);
+            return response()->json([
+                'message' => 'User registered successfully',
+                'status' => 200,
+                'data' => [
+                    'username' => $user->name,
+                    'email' => $user->email,
+                    'image' => null,
+                    'token' => $token,
+                ],
+            ], Response::HTTP_CREATED);
         } catch (ValidationException $e) {
-            return response()->json(['error' => 'some information are not valid', 'status' => 400], Response::HTTP_BAD_REQUEST);
+
+            return response()->json([
+                'message' => 'some information are not valid',
+                'status' => 400
+            ], Response::HTTP_BAD_REQUEST);
         } catch (\Exception $e) {
             // Log the exception message for debugging
             \Log::error('User registration error: ' . $e->getMessage());
 
-            return response()->json(['error' => $e->getMessage(), 'status' => 500], Response::HTTP_INTERNAL_SERVER_ERROR);
+            return response()->json(
+                ['message' => $e->getMessage(), 'status' => 500],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+
+    public function login(Request $request)
+    {
+        try {
+
+            $validator = Validator::make($request->all(), [
+                'email' => 'required',
+                'password' => 'required'
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(
+                    [
+                        'message' => $validator->errors()->first(),
+                        'status' => 400
+                    ],
+                    Response::HTTP_BAD_REQUEST
+                );
+            }
+
+            $user = User::where('email', $request->email)->first();
+
+            // if the user exists in the DB
+            if ($user && Hash::check($request->password, $user->password)) {
+
+                $token = base64_encode($user->name . ":" . $request->password);
+
+                return response()->json([
+                    "message" => "Login successful! Welcome back.",
+                    "status" => Response::HTTP_OK,
+                    'data' => [
+                        'username' => $user->name,
+                        'email' => $user->email,
+                        'image' => null,
+                        'token' => $token,
+                    ],
+                ]);
+            } else {
+                return response()->json([
+                    "message" => "Wrong email or password",
+                    "status" => Response::HTTP_BAD_REQUEST
+                ]);
+            }
+
+        } catch (ValidationException $e) {
+            return response()->json(
+                [
+                    'message' => $e->getMessage(),
+                    'status' => Response::HTTP_BAD_REQUEST
+                ],
+                Response::HTTP_BAD_REQUEST
+            );
         }
     }
 
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::post('/signup', [UserController::class, 'signup']);
+Route::post('/login', [UserController::class, 'login']);
